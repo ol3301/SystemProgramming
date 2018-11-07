ede38a1001866c0fb4ab23d4fa28d6270134d089
Mazda <ol.kyzy@gmail.com>
2018-11-07 11:36:01 +0300

    using app domain

---
+++ Text visualizer/Text visualizer/Text visualizer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_visualizer
+{
+    public partial class Form1 : Form
+    {
+        Module DrawerModule;
+        object Drawer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(Module dm, object dm1)
+        {
+            DrawerModule = dm;
+            Drawer = dm1;
+
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            DrawerModule.GetType("Drawer.Form1").GetMethod("SetText").Invoke(Drawer,new object[] { s });
+        }
+    }
+}
---
+++ Text visualizer/Text visualizer/UsingAppDomains/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingAppDomains
+{
+    static class Program
+    {
+        static AppDomain Drawer;
+        static AppDomain TextWindow;
+
+        static Assembly DrawerAsm;
+        static Assembly TextWindowAsm;
+
+        static Form DrawerWindow;
+        static Form TextWindowWnd;
+
+
+        static void RunDrawer()
+        {
+            DrawerWindow.ShowDialog();
+            AppDomain.Unload(Drawer);
+        }
+
+        static void RunVisualizer()
+        {
+            TextWindowWnd.ShowDialog();
+            Application.Exit();
+        }
+
+        [STAThread]
+        [LoaderOptimization(LoaderOptimization.MultiDomain)]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            Drawer = AppDomain.CreateDomain("Drawer");
+            TextWindow = AppDomain.CreateDomain("TextWindow");
+
+            DrawerAsm = Drawer.Load(AssemblyName.GetAssemblyName("Drawer.exe"));
+            TextWindowAsm = TextWindow.Load(AssemblyName.GetAssemblyName("Text visualizer.exe"));
+
+            DrawerWindow = Activator.CreateInstance(DrawerAsm.GetType("Drawer.Form1")) as Form;
+            TextWindowWnd = Activator.CreateInstance(TextWindowAsm.GetType("Text_visualizer.Form1"),new object[] 
+            { DrawerAsm.GetModule("Drawer.exe"), DrawerWindow}) as Form;
+
+
+
+            (new Thread(new ThreadStart(RunVisualizer))).Start();
+            (new Thread(new ThreadStart(RunDrawer))).Start();
+
+        }
+    }
+}
